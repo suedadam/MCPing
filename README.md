18f688f36c5d9cb220a288cebe6bd463b6582699
Adam Syed <suedadam@gmail.com>
2015-05-07 20:58:43 -0700

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+MCPing
+=============
+
+This is a program to perform a ping on minecraft servers to get the status, player count and MOTD from a server without having to open Minecraft. The idea of this is to check the status of a minecraft server with ease. 
+
+Compilation
+-------------
+
+Compiling it easy!
+
+Grab the dependencies:
+```bash
+$ github.com/minero/minero/proto/packet
+```
+
+### Lastly ###
+
+```bash
+$ go build
+$ ./main
+```
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+        "bytes"
+        "io"
+        "fmt"
+        "net"
+        "strings"
+        "os"
+        "github.com/minero/minero/proto/packet"
+)
+
+func main() {
+        addr := os.Args[1]
+        _, port, err := net.SplitHostPort(addr)
+        if len(port) <= 0 {
+                addr += ":25565"
+        }
+
+        c, err := net.Dial("tcp", addr)
+        if err != nil {
+                fmt.Printf("Error connecting!\n%s\n", err)
+                return
+        }
+        defer c.Close()
+
+        fmt.Println("Connected to:", c.RemoteAddr())
+
+        p := packet.ServerListPing{Magic: 1}
+        p.WriteTo(c)
+
+        var buf bytes.Buffer
+        io.Copy(&buf, c)
+
+        id, _ := buf.ReadByte()
+
+        if id != packet.PacketDisconnect {
+                fmt.Printf("Unexpected packet id! D:\n")
+                return
+        }
+
+        r := new(packet.Disconnect)
+        r.ReadFrom(&buf)
+
+        s := strings.Split(r.Reason, "\x00")
+
+        fmt.Println("Server version:", s[2])
+        fmt.Printf("Players: %s/%s\n", s[4], s[5])
+        fmt.Println("MOTD:", s[3])
+}
